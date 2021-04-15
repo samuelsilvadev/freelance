79cab6b5b751b96506ec51cfcd28e1d6a40c20ec
Samuel Silva <samuelsilvawb@gmail.com>
2021-04-15 23:44:10 +0100

    feat(frontend): Add option to open and close navigation
    - Increased Navigation features to open/close.
    - Added logic to open and close navigation on header.
    - Added css variables file.

--- frontend/components/header/Header.tsx
+++ frontend/components/header/Header.tsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
+
 import Navigation from "components/navigation";
 
+import styles from "./header.module.scss";
+
 function Header(): JSX.Element {
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+  const [isGteMedium, setIsGteMedium] = useState(false);
+
+  const handleToggle = () => {
+    setIsNavigationOpen(!isNavigationOpen);
+  };
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia("(min-width: 1024px)");
+
+    const handleChange = (
+      internalMediaQueryList: MediaQueryList | MediaQueryListEvent
+    ) => {
+      const isViewportMatchesMediumMediaQuery = internalMediaQueryList.matches;
+
+      setIsGteMedium(isViewportMatchesMediumMediaQuery);
+
+      if (!isViewportMatchesMediumMediaQuery) {
+        setIsNavigationOpen(false);
+      }
+    };
+
+    handleChange(mediaQueryList);
+
+    mediaQueryList.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsNavigationOpen(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <header>
-      <Navigation />
+    <header className={styles.wrapper}>
+      {!isGteMedium && (
+        <button className={styles.toggle} onClick={handleToggle}>
+          {!isNavigationOpen ? "open" : "close"}
+        </button>
+      )}
+      <Navigation isOpen={isNavigationOpen || isGteMedium} />
     </header>
   );
 }
--- frontend/components/navigation/Navigation.tsx
+++ frontend/components/navigation/Navigation.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 
 import styles from "./navigation.module.scss";
 
+type Props = {
+  isOpen: boolean;
+};
+
 export const MENU_ITEMS = [
   "home",
   "about",
@@ -11,22 +15,24 @@ export const MENU_ITEMS = [
   "experience",
 ];
 
-function Navigation(): JSX.Element {
-  const renderMenuItem = (item: string) => {
-    return (
-      <li className={styles.listItem} key={item}>
-        <Link href={`#${item}`}>
-          <a className={styles.link}>{item}</a>
-        </Link>
-      </li>
-    );
-  };
-
+const renderMenuItem = (item: string) => {
   return (
+    <li className={styles.listItem} key={item}>
+      <Link href={`#${item}`}>
+        <a className={styles.link}>{item}</a>
+      </Link>
+    </li>
+  );
+};
+
+function Navigation(props: Props): JSX.Element | null {
+  const { isOpen } = props;
+
+  return isOpen ? (
     <nav className={styles.wrapper}>
       <ul className={styles.list}>{MENU_ITEMS.map(renderMenuItem)}</ul>
     </nav>
-  );
+  ) : null;
 }
 
 export default Navigation;
