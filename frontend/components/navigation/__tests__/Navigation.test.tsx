204a549709135e0d2d9363ce7e659f10181b7167
Samuel Silva <samuelsilvawb@gmail.com>
2021-04-19 21:28:10 +0100

    test(frontend): Add test case to closed navigation

--- frontend/components/navigation/__tests__/Navigation.test.tsx
+++ frontend/components/navigation/__tests__/Navigation.test.tsx
@@ -4,10 +4,18 @@ import Navigation, { MENU_ITEMS } from "../Navigation";
 
 describe("Navigation", () => {
   it("should render all items correctly", () => {
-    render(<Navigation />);
+    render(<Navigation isOpen />);
 
     MENU_ITEMS.forEach((menuItem) => {
       expect(screen.getByText(menuItem)).toBeVisible();
+    });
+  });
+
+  it("should not render if it's closed", () => {
+    render(<Navigation />);
+
+    MENU_ITEMS.forEach((menuItem) => {
+      expect(screen.queryByText(menuItem)).not.toBeInTheDocument();
     });
   });
 });
