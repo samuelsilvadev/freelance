a14c223634ff2532b73268e4dde20d33b7a95f00
Samuel Silva <samuelsilvawb@gmail.com>
2021-05-05 21:02:57 +0100

    feat(frontend): Add ProgressBar component

---
+++ frontend/components/progress-bar/ProgressBar.tsx
@@ -0,0 +1,36 @@
+import { SPACE_REGEX } from "utils/regexes";
+
+import type { Props } from "./types";
+
+import styles from "./progressBar.module.scss";
+
+function buildId(value: string): string {
+  return value.trim().replace(SPACE_REGEX, "-").toLowerCase();
+}
+
+function ProgressBar(props: Props): JSX.Element {
+  const { label, value, max = 100 } = props;
+
+  const id = buildId(label);
+
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      <div className={styles.progressWrapper}>
+        <span
+          aria-hidden
+          className={styles.bar}
+          style={{ left: `${value}%` }}
+        />
+        <p className={styles.value}>{value}%</p>
+        <progress className={styles.progress} id={id} value={value} max={max}>
+          {value}%
+        </progress>
+      </div>
+    </div>
+  );
+}
+
+export default ProgressBar;
---
+++ frontend/components/progress-bar/types.ts
@@ -0,0 +1,8 @@
+export type Props = {
+  value: number;
+  label: string;
+  /**
+   * @default 100
+   */
+  max?: number;
+};
